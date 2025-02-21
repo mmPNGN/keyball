fe34ff80ac84932aa33415ef9e7f5052eca88bcc
mmPNGN <y.yokoyama428@gmail.com>
2025-02-21 20:14:30 +0900

    反応時間変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/my_via/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_universal(
     _______ , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______ , KC_1     , KC_2     , KC_3     , KC_4     , KC_5  ,                                     KC_6     , KC_7     , KC_8      , KC_9    , KC_0     , KC_F12   ,
-    _______ , _______  , _______  , KC_BTN1  , KC_BTN2  ,KC_BTN3,                                     KC_PGUP  , KC_BTN1  , KC_BTN2  , _______  , KC_BTN3  , _______  ,
+    _______ , _______  , KC_BTN3  , KC_BTN1  , KC_BTN2  ,_______,                                     KC_PGUP  , KC_BTN1  , KC_BTN2  , _______  , KC_BTN3  , _______  ,
     _______  , _______  , _______ , _______  , _______  ,_______, S(KC_8)  ,               S(KC_9)  , KC_PGDN  , _______  , KC_BTN4  , KC_BTN5  , _______  , _______  ,
     _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
