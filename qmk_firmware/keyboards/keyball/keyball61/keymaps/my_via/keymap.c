562e6fac5dd32d09f6d04103d9d7dcda568ef396
mmPNGN <111752732+mmPNGN@users.noreply.github.com>
2025-04-13 17:41:11 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/my_via/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_SCLN  ,
     KC_LSFT  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  , KC_QUOT  ,
     KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
-    _______  , KC_LWIN  , KC_LALT  , KC_F7,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),          KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_F2  , KC_DEL
+    KC_BTN1  , KC_LWIN  , KC_LALT  , KC_F7,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),          KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_F2  , KC_DEL
   ),
 
   [1] = LAYOUT_universal(
